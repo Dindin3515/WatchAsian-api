e1becdc1bc108dabeafbe56c70d0792fcd8e0690
Hritik R <35923605+HritikR@users.noreply.github.com>
2024-08-15 10:48:19 +0530

    Add logger to all endpoints

--- controllers/shows.js
+++ controllers/shows.js
@@ -1,8 +1,10 @@
 import * as service from "../services/shows.js";
+import { logger } from "../utils/log.js";
 
 export const handleGetAllShows = async (req, res) => {
     try {
         const page = req.query.page || 1;
+        logger.info(`Getting all shows page ${page}`);
         const shows = await service.getAllShows(page);
         res.send(shows);
     } catch (error) {
@@ -13,6 +15,8 @@ export const handleGetAllShows = async (req, res) => {
 export const handleGetShowById = async (req, res) => {
     try {
         const id = req.params.id;
+        if(!id || id === "") throw Error("Missing required parameter");
+        logger.info(`Getting show with id ${id}`);
         const show = await service.getShowById(id);
         res.send(show);
     } catch (error) {
@@ -24,6 +28,7 @@ export const handleGetEpisodeById = async (req, res) => {
     try {
         const id = req.params.id;
         if(!id || id === "") throw Error("Missing required parameter");
+        logger.info(`Getting episode with id ${id}`);
         const show = await service.getEpisodeById(id);
         res.send(show);
     } catch (error) {
@@ -35,6 +40,7 @@ export const handleSearch = async (req, res) => {
     try {
         const keyword = req.query.keyword;
         if(!keyword || keyword === "") throw Error("Missing required parameter");
+        logger.info(`Searching for ${keyword}`);
         const shows = await service.search(keyword);
         res.send(shows);
     } catch (error) {
